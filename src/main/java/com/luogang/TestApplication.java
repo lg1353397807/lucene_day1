9f92bed64f1f60701a36b34a1facd1cf8a9bfd87
luogang <luogang153@163.com>
2019-04-17 20:06:54 +0800

    第一次添加

--- src/main/java/com/luogang/TestApplication.java
+++ src/main/java/com/luogang/TestApplication.java
@@ -8,6 +8,7 @@ public class TestApplication {
 
     public static void main(String[] args) {
         System.out.println("123123123");
+        System.out.println("123123123");
         SpringApplication.run(TestApplication.class, args);
     }
 
